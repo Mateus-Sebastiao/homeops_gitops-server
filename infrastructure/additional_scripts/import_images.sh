92201fbf04747c52363af64bd1e2197f4532bce3
Mateus-Sebastiao <mateussebastiao2003@gmail.com>
2026-04-28 11:11:51 +0200

    Update additional scripts with ollama image

--- infrastructure/additional_scripts/import_images.sh
+++ infrastructure/additional_scripts/import_images.sh
@@ -9,6 +9,7 @@ IMAGES=(
   "quay.io/cilium/hubble-ui-backend:v0.13.3"
   "quay.io/cilium/cilium:v1.19.3"
   "quay.io/cilium/cilium-envoy:v1.36.6-1776000132-2437d2edeaf4d9b56ef279bd0d71127440c067aa"
+  "ollama/ollama:0.21.3-rc0"
 )
 
 for img in "${IMAGES[@]}"; do
